472e0b9f68800d10e27cd7b98a37409429e59129
Pratik Joshi <f2015019@goa.bits-pilani.ac.in>
2017-11-27 12:34:23 +0530

    Modelapi working

--- modelapi.py
+++ modelapi.py
@@ -6,14 +6,15 @@ import matplotlib.pyplot as plt
 import pickle
 import keras
 from keras.models import load_model
+from PIL import Image
 
 base_list = [2310, 2311, 2312, 2313, 2315, 2319, 2325, 2327, 2328, 2330, 2331,
        2332, 2334, 2335, 2336, 2337, 2338, 2340, 2341, 2342, 2343, 2344,
        2346, 2347, 2348, 2349, 2350, 2351, 2352, 2353, 2354, 2357, 2358,
        2359, 2360, 2361, 2384, 2392, 2399, 2404, 2405, 2406, 2407, 2408,
        2409, 2410, 2411, 2412, 2413, 2414, 2415, 2424, 2429]
-matra_list = [0, 2306, 2362, 2363, 2364, 2366, 2367, 2368, 2369, 2370, 2372,
-       2375, 2376, 2379, 2380, 2382, 2387, 2390]
+matra_list = [0, 2363, 2366, 2367, 2368, 2369, 2370, 2372, 2375, 2376, 2379,
+       2380, 2382, 2384, 2387, 2390]
 dot_list = [0, 1]
 
 def pad_resize(img):
@@ -27,30 +28,36 @@ def pad_resize(img):
     return img
 
 def preprocess(img):
-    img = cv2.cvtColor(img, cv2.COLOR_RGBA2GRAY)
+    #plt.imsave("temp.png",img)
+    #img = cv2.imread(os.path.join('','temp.png'),0)
+    img = cv2.cvtColor(img,cv2.COLOR_RGBA2GRAY)
     blur = cv2.GaussianBlur(img,(9,9),0)
     a,img = cv2.threshold(blur,127,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    plt.imshow(img,'gray')
+    plt.show()
     img = pad_resize(img)
-
+    return img
 
 def predict(img):
+
     img = preprocess(img)
     X = np.reshape(img, (-1, 64*64))
     X_test = np.reshape(img,(-1,64,64,1))
 
-    base_model = load_model('base_model.h5')
-    matra_model = load_model('matra_model.h5')
-    dot_model = load_model('dot_model.h5')
+    base_model = load_model('base_model')
+    matra_model = load_model('matra_model')
+    dot_model = load_model('dot_model')
 
-    base_pred = base_list[np.argmax(model.predict(X_test))]
-    matra_pred = matra_list[np.argmax(model.predict(X_test))]
-    dot_pred = dot_list[np.argmax(model.predict(X_test))]
+    base_pred = base_list[np.argmax(base_model.predict(X_test))]
+    matra_pred = matra_list[np.argmax(matra_model.predict(X_test))]
+    dot_pred = dot_list[np.argmax(dot_model.predict(X_test))]
 
     total_pred = [base_pred,matra_pred,dot_pred]
 
-    total_pred = filter(lambda a : a != 0, total_pred)
+    #total_pred = [i for i in total_pred if i!=0]
+    print("Integers" + str(total_pred))
+    print("Hex" + str([hex(i) for i in total_pred]))
     return total_pred
 
-im = plt.imread('ok.png')
-print(predict(im))
-
+im = cv2.imread('ok.png')
+predict(im)
